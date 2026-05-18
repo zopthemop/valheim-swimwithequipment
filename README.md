b063f08130849c053cda88289c2ba857e5cb5d6f
zopthemop <23084398+zopthemop@users.noreply.github.com>
2026-05-18 17:59:40 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Small Valheim mod that makes you able to swim with equipment on (e.g. the game
+doesn't force you to unequip right and left hand items when you swim)
---
+++ main.cs
@@ -0,0 +1,85 @@
+using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace SwimWithEquipment;
+
+[BepInPlugin(ModGUID, ModName, ModVersion)]
+public class Plugin : BaseUnityPlugin
+{
+	public const string ModGUID = "zopthemop.swimwithequipment";
+	public const string ModName = "Swim With Equipment";
+	public const string ModVersion = "1.0.0";
+
+    private void Awake()
+    {
+        Harmony harmony = new(ModGUID);
+        harmony.PatchAll();
+    }
+
+	[HarmonyPatch(typeof(Player), "Update")]
+	public static class Player_Update_Patch
+	{
+		private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+		{
+			MethodInfo targetMethod = AccessTools.Method(typeof(Character), nameof(Character.IsSwimming));
+
+			foreach (CodeInstruction instruction in instructions)
+			{
+				if (instruction.Calls(targetMethod))
+				{
+					yield return new CodeInstruction(OpCodes.Pop); // remove Character object from stack
+					yield return new CodeInstruction(OpCodes.Ldc_I4_0); // replace IsSwimming with false
+					continue;
+				}
+
+				yield return instruction;
+			}
+		}
+	}
+
+	[HarmonyPatch(typeof(Humanoid), "EquipItem")]
+	public static class Humanoid_EquipItem_Patch
+	{
+		private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+		{
+			MethodInfo targetMethod = AccessTools.Method(typeof(Character), nameof(Character.IsSwimming));
+
+			foreach (CodeInstruction instruction in instructions)
+			{
+				if (instruction.Calls(targetMethod))
+				{
+					yield return new CodeInstruction(OpCodes.Pop); // remove Character object from stack
+					yield return new CodeInstruction(OpCodes.Ldc_I4_0); // replace IsSwimming with false
+					continue;
+				}
+
+				yield return instruction;
+			}
+		}
+	}
+
+	[HarmonyPatch(typeof(Humanoid), "UpdateEquipment")]
+	public static class Humanoid_UpdateEquipment_Patch
+	{
+		private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+		{
+			MethodInfo targetMethod = AccessTools.Method(typeof(Character), nameof(Character.IsSwimming));
+
+			foreach (CodeInstruction instruction in instructions)
+			{
+				if (instruction.Calls(targetMethod))
+				{
+					yield return new CodeInstruction(OpCodes.Pop); // remove Character object from stack
+					yield return new CodeInstruction(OpCodes.Ldc_I4_0); // replace IsSwimming with false
+					continue;
+				}
+
+				yield return instruction;
+			}
+		}
+	}
+}
